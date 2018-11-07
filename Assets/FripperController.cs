6e510d216a968d8da825ba949b5953a6887f1586
AtsiKi <a.atsuki0618@gmail.com>
2018-11-07 20:07:19 +0900

    PinBallプロジェクト　課題完了
    得点表示を実装

---
+++ Assets/FripperController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+    private HingeJoint myHingeJoint;
+
+    private float defautAngle = 20;
+
+    private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        SetAngle(this.defautAngle);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag"){
+            SetAngle(this.flickAngle);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defautAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defautAngle);
+        }
+    }
+    public void SetAngle (float angle){
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+
+}
